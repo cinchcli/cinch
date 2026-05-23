fb9b6fbbee072177b522a964eae7cdcbec4ec07d
JinmuGo <jingmuio@gmail.com>
2026-05-23 16:47:35 +0900

    feat(desktop): add tauri-plugin-dialog for save-as dialogs

--- apps/desktop/src-tauri/src/lib.rs
+++ apps/desktop/src-tauri/src/lib.rs
@@ -262,6 +262,7 @@ pub fn run() {
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_deep_link::init())
         .plugin(tauri_plugin_clipboard_manager::init())
+        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_global_shortcut::Builder::new().build())
         .plugin(tauri_plugin_autostart::init(
